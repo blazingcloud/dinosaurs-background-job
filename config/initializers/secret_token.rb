02b3a4ebf733fdf32feff195079e350f815e9c58
Judy Tuan <judytuna@gmail.com>
2012-06-26 16:58:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackgroundSpike::Application.config.secret_token = 'cb972c6c3356c34fe64de5bd5c430251ed75dcdc364940515babce5552e1e6bcd082fc614849fee90e063610bd41b9d6d9e46e4023f9b0f78f205ddbe23a291b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BackgroundSpike::Application.config.session_store :cookie_store, key: '_background_spike_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BackgroundSpike::Application.config.session_store :active_record_store
